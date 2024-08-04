52614a1d7731c31d57e70d2cb31bc4d3d28c491c
mongolyy <mongolyy@gmail.com>
2024-08-04 22:02:55 +0900

    change btn

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './button.css';
 
 type ButtonProps = {
@@ -25,29 +24,6 @@ export const Button: React.FC<ButtonProps> = ({ primary, backgroundColor, size, 
       {label}
     </button>
   );
-};
-
-Button.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  backgroundColor: PropTypes.string,
-  /**
-   * How large should the button be?
-   */
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  /**
-   * Button contents
-   */
-  label: PropTypes.string.isRequired,
-  /**
-   * Optional click handler
-   */
-  onClick: PropTypes.func,
 };
 
 Button.defaultProps = {
